51f9538b32d77f3122f41e5f99191fa5ce5e9a14
leonat <130.0x82@gmail.com>
2020-07-22 01:37:04 +0200

    Update deploy.sh

--- .environment/deploy.sh
+++ .environment/deploy.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# accessible var DEPLOY_REPO_NAME
+# accessible var DEPLOY_DEST
+# accessible var DEPLOY_FROM
+
 echo deploy!
+
+runDeployScript="$DEPLOY_DEST"/../run-deploy.sh
+
+sudo chown root:root "$runDeployScript"
+sudo ln -s "$runDeployScript" /usr/local/sbin/run-deploy
