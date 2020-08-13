dc7ddff6a675b85d774c4ac0f0e063032970f552
leonat <130.0x82@gmail.com>
2020-08-13 16:42:36 +0200

    Create libreimport.sh

---
+++ libreimport.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+file="${1}"
+
+if [[ $file = *.wav ]];then
+	echo "convert to mp3"
+	mkdir -p /tmp/libreimport
+
+	newFile=/tmp/libreimport/$(basename "$file" .wav).mp3
+	ffmpeg -i "$file" -codec:a libmp3lame -qscale:a 0 "${newFile}"
+	file="${newFile}"
+fi
+
+
+python - << EOF
+
+import sys
+import json
+import time
+import select
+import signal
+import os
+import logging
+import urllib3
+pathname = "${file}"
+api_key = ""
+url = 'http://studio.radioangrezi.de/rest/media'
+http = urllib3.PoolManager()
+with open (pathname, "rb") as audiofile:
+    audio_data = audiofile.read()
+path, filename = os.path.split(pathname)
+authstring = str(api_key) + ':'
+print(filename)
+headers = urllib3.util.make_headers(basic_auth=authstring)
+fields = {'file': (filename, audio_data)}
+r = http.request('POST', url, headers=headers, fields=fields)
+print(r.status)
+#TODO we might want to parse r.text to determine if the upload status = 1 and was successful then delete
+
+EOF
